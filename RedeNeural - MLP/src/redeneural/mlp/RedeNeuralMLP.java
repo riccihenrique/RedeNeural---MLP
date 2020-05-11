c7d4c4b9a6d78c175ac518f5e32fc5dd7cca2d67
Henrique <ricci.henrique@outlook.com>
2020-05-11 20:42:31 -0300

    Inicial

---
+++ RedeNeural - MLP/src/redeneural/mlp/RedeNeuralMLP.java
@@ -0,0 +1,24 @@
+package redeneural.mlp;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class RedeNeuralMLP extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLMain.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
